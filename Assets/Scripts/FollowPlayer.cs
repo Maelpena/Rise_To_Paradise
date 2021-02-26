b66a77acfd0e399b988c1baf8cc2a0cc5fb4c0bb
Mael <pena.mael@gmail.com>
2021-02-26 15:04:26 +0100

    shake

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -17,6 +17,13 @@ public class FollowPlayer : MonoBehaviour
         if(Player != null)
         {
             gameObject.transform.position = new Vector3(gameObject.transform.position.x, Player.transform.position.y, gameObject.transform.position.z);
-        }        
+        }
+        shakeScreen();
+    }
+
+    public void shakeScreen()
+    {
+        Vector3 randomPoint = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), transform.position.z);
+        transform.position = randomPoint;
     }
 }
