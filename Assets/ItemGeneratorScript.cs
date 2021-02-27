f5f9d70512e9d3a7682ebc546a485f70c5b8605a
Peio <rouyre.pierre@outlook.fr>
2021-02-27 17:13:11 +0100

    Spawn Power Up

--- Assets/ItemGeneratorScript.cs
+++ Assets/ItemGeneratorScript.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 using UnityEngine.Events;
 public class ItemGeneratorScript : MonoBehaviour
 {
-    public UnityEvent enemyDied;
-    public List<GameObject> powerUps = new List<GameObject>();
+    public float baseLuckSpawnItem = 5f;
+    public float CurrentLuck = 5f;
+    public float LuckPercent = 100f;
+    public List<GameObject> lPowerUps = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
 
+    }
+    private void OnEnable()
+    {
+        CharacterData.EventEnemyDeath.AddListener(EnemyDied);
     }
 
     // Update is called once per frame
@@ -18,13 +24,28 @@ public class ItemGeneratorScript : MonoBehaviour
 
     }
 
-    public void EnemyDied()
+    public void EnemyDied(float score,Vector2 position,GameObject Levelpart)
     {
-        Debug.Log("ENEMY Died");
+        Debug.Log("EnemyDied " + score);
+        float luckValue = 0.0f;
+        luckValue = Random.Range(0.0f, LuckPercent);
+        if (luckValue <= CurrentLuck)
+        {
+            Debug.Log("SPAWN POWER UP");
+            SpawnPowerup(position,Levelpart);
+            CurrentLuck = baseLuckSpawnItem;
+        }
+        else
+        {
+            CurrentLuck +=(score / 2) / 10;
+        }
+        
     }
-
-    public void eventDeath()
+    void SpawnPowerup(Vector2 position, GameObject LevelPart)
     {
-
+        GameObject PU;
+        PU = Instantiate(lPowerUps[Random.Range(0, lPowerUps.Count)]);
+        PU.transform.parent = LevelPart.transform;
+        PU.transform.position = position;
     }
 }
