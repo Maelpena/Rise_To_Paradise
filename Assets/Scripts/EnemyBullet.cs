6bd85f080e8bc07e9e3be9376262f6815b67b85b
Louis de Nombel <louis.de-nombel@epitech.eu>
2021-03-08 16:04:35 +0100

    Shooting enemy V1

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : Bullet
+{
+    private int damage = 1;
+
+    private Transform player;
+    private Vector2 target;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(player.position.x, player.position.y);
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (transform.position.x == target.x && transform.position.y == target.y)
+            DestroyEnemyBullet();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (lMaskCollision == (lMaskCollision | (1 << collision.gameObject.layer)))
+        {
+
+            if (collision.gameObject.tag.Equals("Player"))
+            {
+                collision.gameObject.GetComponent<Player>().TakeDamage(damage);
+                if (bulletCrossing)
+                    return;
+                anim.Play("Hitting");
+            }
+            else
+            {
+                anim.Play("Splashing");
+            }
+            dir.x = 0;
+            dir.y = 0;
+        }
+    }
+
+    void DestroyEnemyBullet()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ShootingEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ShootingEnemy : Enemy
+{
+    [SerializeField] public GameObject BalleType;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    public GameObject enemyBullet;
+    private Transform player;
+
+    void Start()
+    {
+        timeBtwShots = startTimeBtwShots;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(enemyBullet, null);
+            enemyBullet.transform.position = gameObject.transform.position;
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+            timeBtwShots -= Time.deltaTime;
+    }
+}
